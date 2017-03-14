61734281278e5ef5041beee6783f28486a0a4fa3
Zhong Kang <1176011257@qq.com>
2017-03-14 23:49:37 +0800

    add config directory

--- database/manage.py
+++ database/manage.py
@@ -3,10 +3,10 @@ from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app
-from model import db
+from model import db as db_sqlite
 
 manager = Manager(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db_sqlite)
 manager.add_command('db', MigrateCommand)
 
 
